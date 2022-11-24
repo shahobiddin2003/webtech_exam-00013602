95a3c459f032e71b8a5eb700bb5e4939640f813b
shahobiddin2003 <108112545+shahobiddin2003@users.noreply.github.com>
2022-11-24 20:52:04 +0500

    Add files via upload
    uploaded

---
+++ webtech_portfolio/README.md
@@ -0,0 +1,3 @@
+# About the project
+
+This is a 7 page portfolio website prepared for Web Technology module Coursework. the Figma design was used for creating this website. The whole portfolio is consists of 3 type of documents: HTML, CSS, JavaScript. Each page has its own HTML file and CSS file. There is only one JavaScript file, which is used for linking pages with each other and logos with original websites. the portfolio has its Home page, the Portfolio page itself, four pages for Four Categories, one Contact page.
---
+++ webtech_portfolio/js/homescript.js
@@ -0,0 +1,33 @@
+function GoToPortfolio() {
+  window.location = "../html/Portfolio_index.html";
+}
+function GoToContacts() {
+  window.location = "../html/contacts.html";
+}
+function goToHome() {
+  window.location = "../html/home.html";
+}
+function goToBookmark() {
+  window.location = "../html/bookmark.html";
+}
+function goToInsure() {
+  window.location = "../html/insure.html";
+}
+function goToFylo() {
+  window.location = "../html/Flyo.html";
+}
+function goToManage() {
+  window.location = "../html/manage.html";
+}
+
+function goToGithub() {
+  location.href = "https://github.com/";
+}
+
+function goToTwitter() {
+  location.href = "https://twitter.com/";
+}
+
+function goToLinkedn() {
+  location.href = "https://www.linkedin.com/";
+}
